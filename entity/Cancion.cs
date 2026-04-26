8a2f3c70a87144ee7a9da82d6bd0522ca4b5ce62
Paulo Espinoza Gamboa <paulollito29@gmail.com>
2026-04-26 14:39:19 -0500

    Deja entidades como modelos planos y mueve lógica al controlador

---
+++ entity/Cancion.cs
@@ -0,0 +1,9 @@
+namespace FUSIIN_S2_Ejercicio1.entity
+{
+    internal class Cancion
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public double Duracion { get; set; }
+    }
+}
